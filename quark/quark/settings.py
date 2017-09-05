7a8a8f4c7b0c11772ef1018731b7cb98dc966037
jiaxin <edison7500@gmail.com>
2017-09-05 16:20:25 +0800

    update

--- quark/quark/settings.py
+++ quark/quark/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@qai(b0#l9nfe%1e9upy1ur5*8$&^-=gd%!5=km$(ui0e0$h^r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["45.55.187.161", ]
 
 
 # Application definition
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'bootstrap3',
+    'web',
 ]
 
 MIDDLEWARE = [
