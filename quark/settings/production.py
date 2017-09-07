1a4d6693b68f44a753cc047a95e097370bddfcca
laohanmsa <anchen@guoku.com>
2017-09-08 03:54:23 +0800

    update

--- quark/settings/production.py
+++ quark/settings/production.py
@@ -6,7 +6,7 @@ ALLOWED_HOSTS = ["www.bit03.com", "bit03.com"]
 
 '''session
 '''
-SESSION_ENGINE          = 'django.contrib.sessions.backends.file'
+SESSION_ENGINE          = 'django.contrib.sessions.backends.cache'
 SESSION_COOKIE_DOMAIN   = ".bit03.com"
 
 CACHES = {
