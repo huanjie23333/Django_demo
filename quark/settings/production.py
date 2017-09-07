b347564dbd7edb4070d576c58d9f50809fba6eea
jiaxin <edison7500@gmail.com>
2017-09-07 16:37:02 +0800

    merge

--- quark/settings/production.py
+++ quark/settings/production.py
@@ -21,6 +21,21 @@ CACHES = {
     },
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'baryon',
+        'USER': 'baryon',
+        'PASSWORD': 'baryon1@#',
+        'HOST': '10.132.11.59',
+        'PORT': 3306,
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET storage_engine=INNODB, sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
+
 
 COMPRESS_ENABLED = True
 
