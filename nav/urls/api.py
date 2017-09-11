40933b2abf5af27fa75c0284c4ac70e9bcca46ca
jiaxin <edison7500@gmail.com>
2017-09-11 20:47:04 +0800

    update

--- nav/urls/api.py
+++ nav/urls/api.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from nav.views.api import DappsListAPIView
+from nav.views.api import DappsListAPIView, DappsDetailAPIView
 
 
 urlpatterns = [
-    url(r'^$', DappsListAPIView.as_view(), name='list')
+    url(r'^$', DappsListAPIView.as_view(), name='list'),
+    url(r'^(?P<slug>\d+)/?$', DappsDetailAPIView.as_view(), name='detail'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- nav/views/api.py
+++ nav/views/api.py
@@ -1,11 +1,16 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from nav.serializers import DappsSerializer
 from nav.models import Dapps
 
 
-class DappsListAPIView(ListAPIView):
+class DappsListAPIView(ListCreateAPIView):
     serializer_class = DappsSerializer
     queryset = Dapps.objects.all()
     model = Dapps
 
-    
+
+class DappsDetailAPIView(RetrieveAPIView):
+    serializer_class = DappsSerializer
+    queryset = Dapps.objects.all()
+    model = Dapps
+    lookup_field = 'slug'
