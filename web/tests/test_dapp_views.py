22c022ffacb5d1b0a189f9dd1a982f68fdfbc9f3
laohanmsa <anchen@guoku.com>
2017-09-29 19:37:23 +0800

    update dapp detail page

--- web/tests/test_dapp_views.py
+++ web/tests/test_dapp_views.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.urls import reverse
 from factory.django import DjangoModelFactory
 
+from nav.models import Project
+
 
 class ProjectFactory(DjangoModelFactory):
     class Meta:
@@ -16,14 +18,26 @@ class TestDappViewTestCase(TestCase):
                        software_license = 'GPL', \
                        )
 
-
     def test_dapp_list_view(self):
-
         resp = self.client.get(reverse('dapp:list'))
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(resp, 'dapp/list.html')
         self.assertTemplateUsed(resp, 'dapp/partial/dapp_list_meta.html')
         self.assertContains(resp, 'proj_name_1')
+
+
+    def test_dapp_detail_view(self):
+        p = Project.objects.all()[0]
+        resp = self.client.get(reverse('dapp:detail', args=[p.slug, ]))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'dapp/detail.html')
+        self.assertTemplateUsed(resp, 'dapp/patial/dapp_detail_meta.html')
+        self.assertContains(resp, 'fd_2')
+
+
+
+
+
 
 
 
--- web/views/dapp.py
+++ web/views/dapp.py
@@ -8,3 +8,10 @@ class DAppListView(generic.ListView):
     template_name = 'dapp/list.html'
     context_object_name = 'projects'
     pass
+
+
+class DAppDetailView(generic.DetailView):
+    queryset = Project.objects.all()
+    template_name = 'dapp/detail.html'
+    context_object_name = 'project'
+    slug_field = 'slug'
