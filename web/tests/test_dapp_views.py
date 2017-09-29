4eed4734d565d93d7df73b5595fe8edf959d4b08
laohanmsa <anchen@guoku.com>
2017-09-29 12:19:57 +0800

    update test

--- web/tests/test_dapp_views.py
+++ web/tests/test_dapp_views.py
@@ -1,10 +1,28 @@
 from django.test import TestCase
 from django.urls import reverse
+from factory.django import DjangoModelFactory
+
+
+class ProjectFactory(DjangoModelFactory):
+    class Meta:
+        model = 'nav.models.Project'
+        django_get_or_create = 'name'
 
 
 class TestDappViewTestCase(TestCase):
+    def setUp(self):
+        ProjectFactory(slug='slug_1', state='normal', \
+                       name='proj_name_1', founder='fd_1', \
+                       software_license = 'GPL', \
+                       )
+
+
     def test_dapp_list_view(self):
+
         resp = self.client.get(reverse('dapp:list'))
         self.assertEqual(resp.status_code, 404)
-        self.assertTemplateUsed(resp, 'web/')
+        self.assertTemplateUsed(resp, 'dapp/list.html')
+        self.assertTemplateUsed(resp, 'dapp/partial/dapp_list_meta.html')
+        self.assertContains(resp, 'proj_name_1')
+
 
--- web/views/dapp.py
+++ web/views/dapp.py
@@ -1,5 +1,10 @@
 from django.views import generic
 
+from nav.models import Project
+
+
 class DAppListView(generic.ListView):
+    queryset = Project.objects.all()
     template_name = 'dapp/list.html'
+    context_object_name = 'projects'
     pass
