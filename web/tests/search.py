9206c83a4a5661d50dc0f83f7517627cfbd0229b
jiaxin <edison7500@gmail.com>
2017-09-29 14:16:48 +0800

    update

---
+++ web/tests/search.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+
+
+class SearchViewTestCase(TestCase):
+
+    def test_can_get_search_result(self):
+        _url = reverse("search:nav")
+
+        res = self.client.get(_url, data={"q":"coin"})
+        self.assertIs(res.status_code, status.HTTP_200_OK)
+        self.assertTemplateUsed(res, "search/search.html")
+
