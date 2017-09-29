95f8253546d893293c866cc385733dfbc3236298
jiaxin <edison7500@gmail.com>
2017-09-29 13:56:19 +0800

    update

--- nav/search_indexes.py
+++ nav/search_indexes.py
@@ -15,6 +15,11 @@ class NavIndex(indexes.Indexable, indexes.SearchIndex):
     alias = indexes.CharField(model_attr='alias', default='')
     cate_id = indexes.FacetIntegerField(model_attr='cate_id')
 
+    created_at = indexes.DateTimeField(model_attr='created_at')
+    updated_at = indexes.DateTimeField(model_attr='updated_at')
+
+    main_name_auto = indexes.EdgeNgramField(model_attr='main_name')
+
     def get_model(self):
         return Nav
 
