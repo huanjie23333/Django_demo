c301462365ca4d5f5bb692a2b061774f9737c4e7
laohanmsa <anchen@guoku.com>
2017-10-11 12:39:18 +0800

    add feed test

--- quark/settings/dev_anchen.py
+++ quark/settings/dev_anchen.py
@@ -15,7 +15,7 @@ INSTALLED_APPS += [
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 NOSE_ARGS = [
     '--with-coverage',
-    '--cover-package=web,nav,quark',
+    '--cover-package=web,nav,quark,feed',
 ]
 
 
