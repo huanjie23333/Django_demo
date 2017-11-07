34912ea647023029d03b1579f789adae1e279832
肖成胤 <niseitane@xiaocheyindeMBP.lan>
2017-11-07 16:23:52 +0800

    tag trigger

--- static/js/web/jsbuild/index_page_app_build.js
+++ static/js/web/jsbuild/index_page_app_build.js
@@ -3788,13 +3788,14 @@ define('subapp/tools/bookmark',['jquery'], function($){
 define('subapp/news/tagtrigger',['jquery','libs/Class'],function ($, Class) {
    var TagTrigger = Class.extend({
        init: function(){
-           var $btn = $('.tag-list-filter-bottom a');
-           var $btnIcon = $('.tag-list-filter-bottom i');
+           var $btn = $('.tag-list-filter-list a');
+           var $btnIcon = $('.tag-list-filter-list i');
            var $ul = $('ul.tag-list-filter-list');
            $ul.addClass('tag-list-filter-list-hide');
            $btn.on('click', function(){
                $ul.toggleClass('tag-list-filter-list-hide');
-               $btnIcon.toggleClass('fa-angle-double-down').toggleClass('fa-angle-double-up');
+               $btnIcon.toggleClass('icon-rotate');
+               // $btnIcon.toggleClass('fa-sort-down').toggleClass('fa-sort-up');
            });
        }
    });
