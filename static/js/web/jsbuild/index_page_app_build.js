feb9a160f320915ce017fcdf452c4a6dc5873039
xiaochy <xchy@chaindh.com>
2017-11-14 18:14:02 +0800

    url验证

--- static/js/web/jsbuild/index_page_app_build.js
+++ static/js/web/jsbuild/index_page_app_build.js
@@ -3762,12 +3762,21 @@ define('subapp/submit/getsitedata',['jquery','libs/Class', 'libs/csrf'], functio
 
     var GetSiteData = Class.extend({
         init: function(){
+            var $dom = $('#id_web_site').after('<div class="url-error">网址不合法</div>')
+                .parent('div').css('position', 'relative');
+            $('.url-error').hide();
             $('#id_web_site').on('blur', function(){
                 var url = location.protocol + '//' + location.host + '/tools/site/?url=' + $(this).val();
-                $.getJSON(url, function(data){
-                    $('#id_cname').val(data.title);
-                    $('#id_description').val(data.description);
-                });
+                var reg = /^(http:\/\/www.|https:\/\/www.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)_.[a-z]{2,5}(:[0-9]{1,5})?(\/._)?$/;
+                if(reg.test($(this).val())){
+                    $('.url-error').hide();
+                    $.getJSON(url, function(data){
+                        $('#id_cname').val(data.title);
+                        $('#id_description').val(data.description);
+                    });
+                } else {
+                    $('.url-error').show();
+                }
             });
         }
     });
