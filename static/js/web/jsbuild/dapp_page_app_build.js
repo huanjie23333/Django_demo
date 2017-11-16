3271a548bf8e45ac2d220fe0d76a89a8bc9372d8
xiaochy <xchy@chaindh.com>
2017-11-16 15:31:55 +0800

    搜快讯autocomplete

--- static/js/web/jsbuild/dapp_page_app_build.js
+++ static/js/web/jsbuild/dapp_page_app_build.js
@@ -1346,11 +1346,19 @@ define('subapp/header/search',['libs/Class', 'jquery', 'underscore','libs/autoco
                     source: function (term, response) {
                         try { xhr.abort(); } catch(e){}
                         $.getJSON('/search/autocomplete/', {q: term},
-                            function (data) {
-                            response(data['results']);
-                        });
+                            function(data){
+                            // response(data['results']);
+                                $.ajax('http://www.chainscoop.com/api/news/autocomplete/', {
+                                    jsonp: true,
+                                    success: function(data2){
+                                        var results = data.results.concat(data2.results);
+                                        response(results);
+                                    },
+                                    method: 'GET',
+                                    data: {q: term}
+                                });
+                        })
                     }
-
                 });
                 console.log('search');
 
