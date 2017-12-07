f82ac2d698db6de0fcc4a87f18c71247a8ab1761
jiaxin <edison7500@gmail.com>
2017-12-07 12:21:16 +0800

    update

--- flink/models.py
+++ flink/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.utils.functional import cached_property
 
+
 # Create your models here.
 
 class Flink(models.Model):
