800d2123fe28f08383fa5daa227cc49bc9993e8c
laohanmsa <laohan.msa@gmail.com>
2017-12-12 16:25:37 +0800

    update

--- coinfork/models.py
+++ coinfork/models.py
@@ -2,16 +2,34 @@ from django.db import models
 from django.utils.translation import gettext as _
 
 # Create your models here.
+from model_utils import Choices
+from model_utils.fields import StatusField
+
 
 class CoinFork(models.Model):
-    coin_name = models.CharField(max_length=128, null=False, blank=False)
-    coin_ename = models.CharField(max_length=128,null=True, blank=True)
-    fork_name = models.CharField(max_length=128, null=False, blank=False)
-    fork_ename = models.CharField(max_length=128, null=True, blank=True)
-    block_mining_time = models.IntegerField(help_text=_("平均出块时间, mint"))
+    STATUS = Choices('removed', 'done', 'incoming')
+
+    coin_name = models.CharField(max_length=64, null=False, blank=False)
+    coin_ename = models.CharField(max_length=64,null=True, blank=True)
+    fork_name = models.CharField(max_length=64, null=False, blank=False)
+    fork_ename = models.CharField(max_length=64, null=True, blank=True)
+    fork_alias = models.CharField(max_length=32, null=True, blank=True)
+
+    status = StatusField(_('status'), choices_name='STATUS', default=STATUS.incoming)
+
+    block_mining_time = models.IntegerField(help_text=_("平均出块时间, mint"), default=10)
     fork_height = models.IntegerField(help_text=_("分叉高度"))
     current_height_api = models.URLField(help_text=_("当前区块高度API"), default='https://blockchain.info/q/getblockcount')
     description = models.TextField(default='', blank=True)
+    official_site = models.URLField(blank=True, null=True)
+    related_article = models.URLField(blank=True, null=True)
+
+
+
+
+
+
+
 
 
 
