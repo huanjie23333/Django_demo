186a7a188927e15d56922f17a6256b4e8f9bf9c1
laohanmsa <laohan.msa@gmail.com>
2017-12-15 14:12:19 +0800

    update

--- static/js/web/app/subapp/data/btc_forks.js
+++ static/js/web/app/subapp/data/btc_forks.js
@@ -11,14 +11,6 @@ define([],function () {
                     'ename': 'BTC King ',
                     'height':499999
                 },
-                // {
-<<<<<<< HEAD
-=======
-                //     'name': '超级比特币',
-                //     'ename': 'Bitcoin Platinum',
-                //     'height':498888
->>>>>>> f9dd48ac9edb2c6772ed50d117e72681c614bf00
-                // }
             ];
 
     return fork_list;
