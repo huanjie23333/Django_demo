05744dc7a1f880751d5710cbc09660d9285e7abc
laohanmsa <laohan.msa@gmail.com>
2017-12-21 20:38:19 +0800

    update

--- quark/urls.py
+++ quark/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
     url(r'^search/', include('web.urls.search', namespace='search')),
     url(r'^feed/', include('feed.urls', namespace='feed')),
     url(r'^tools/', include('webtools.urls', namespace='tools')),
-    url(r'^d3_test/', D3TestView.as_view(), name='d3_test'),
+    url(r'^token_sale_history/', D3TestView.as_view(), name='d3_test'),
 
 
 ]
@@ -64,8 +64,8 @@ urlpatterns += [
 ]
 
 urlpatterns += [
-    # url(r'^$', IndexView.as_view(), name='web_index'),
-    url(r'^$', cache_page(1800)(IndexView.as_view()), name='web_index'),
+    url(r'^$', IndexView.as_view(), name='web_index'),
+    # url(r'^$', cache_page(1800)(IndexView.as_view()), name='web_index'),
 ]
 
 from django.contrib.flatpages import views
--- static/js/web/jsbuild/index_page_app_build.js
+++ static/js/web/jsbuild/index_page_app_build.js
@@ -1393,17 +1393,68 @@ define('subapp/header/search',['libs/Class', 'jquery', 'underscore','libs/autoco
         return Search;
 
     });
+define('subapp/header/promo_text',['libs/Class','jquery', 'underscore'], function(Class, $, _){
+
+    var PromoTextApp = Class.extend({
+        init: function () {
+            var stop = false;
+            console.log('in promotext');
+
+
+            // $('.promo-text-wrapper').marquee({
+            //     gap: 150,
+            //     duration: 500,
+            // });
+            // window.setInterval(function(){
+            //      if (stop){
+            //        $('.promo-text-wrapper').marquee('toggle');
+            //      }else{
+            //          stop = true;
+            //          return ;
+            //      }
+            //
+            // }, 1000);
+            this.element_count = 4 ;
+            this.current_ele_index = 1 ;
+            this.element_width = 150;
+            this.$ele_wrapper = $('.promo-text-list');
+            if(!this.$ele_wrapper.length){
+                return;
+            }
+            this.start_mq();
+        },
+
+        start_mq: function () {
+            window.setInterval(this.marquee.bind(this), 2000)
+        },
+        marquee: function () {
+            if(this.current_ele_index == this.element_count){
+                this.$ele_wrapper.css({left: -this.element_width * 1});
+                this.current_ele_index = 2;
+            }
+            console.log('current ele index : ' + this.current_ele_index);
+            console.log('move : ' + this.element_width * (this.current_ele_index-1));
+            this.current_ele_index += 1;
+            this.$ele_wrapper.animate({left: -this.element_width * (this.current_ele_index-1)}, 500);
+        }
+
+
+    });
+
+    return PromoTextApp;
+});
 define('subapp/header/header',['libs/Class',
         'subapp/header/header_price',
         'subapp/adapters/HeaderCoinmarketcapAdapter',
         'subapp/header/search',
-
+        'subapp/header/promo_text',
     ],
     function(
         Class,
         HeaderPrice,
         HeaderCoinmarketAdapter,
-        Search
+        Search,
+        PromoText
     ){
 
     var HeaderApp = Class.extend({
@@ -1415,6 +1466,8 @@ define('subapp/header/header',['libs/Class',
             });
 
             this.search = new Search();
+            // for promo text
+            new PromoText();
 
         },
 
