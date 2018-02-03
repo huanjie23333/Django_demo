1a58a9022d6e3b3c2a2ccb7f4aadf8b0eb86134a
jiaxin <edison7500@gmail.com>
2018-02-03 12:51:15 +0800

    update

--- web/templatetags/news.py
+++ web/templatetags/news.py
@@ -1,8 +1,10 @@
 import requests
+import json
 import logging
 from django.conf import settings
 from django import template
-from django.core.cache import cache
+
+# from django.core.cache import cache
 
 register = template.Library()
 
@@ -26,8 +28,13 @@ def show_sidebar_news():
 @register.inclusion_tag("web/snippets/sidebar_news_tags.html")
 def show_sidebar_news_tags():
     tags = list()
+    tags_json_string = ""
     res = requests.get(news_tag_api_url, timeout=5)
     if res.status_code == 200:
         tags = res.json()['tags']
-
-    return {"tag_list": tags}
+        tags_json_string = json.dumps(tags)
+        # sidebar_tag_jsonstr
+    return {
+        "tag_list": tags,
+        "": tags_json_string
+    }
