642f127d97371c975a0344ebdc79207a0de9ef9f
an chen <laohan.msa@gmail.com>
2018-04-27 12:05:27 +0800

    update

--- nav/views/api.py
+++ nav/views/api.py
@@ -6,7 +6,7 @@ from rest_framework import pagination
 
 
 class StandardResultsSetPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 30
     page_size_query_param = 'size'
     max_page_size = 1000
 
