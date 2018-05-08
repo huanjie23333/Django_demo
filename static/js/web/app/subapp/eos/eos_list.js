bdf0d487e9aafa755d24c8df8e47bb8fdde92e86
xiaochy <xchy@chaindh.com>
2018-05-08 23:42:02 +0800

    翻页 动画 入口

--- static/js/web/app/subapp/eos/eos_list.js
+++ static/js/web/app/subapp/eos/eos_list.js
@@ -3,18 +3,38 @@ define(['libs/Class', 'jquery', 'underscore'], function(Class, $, _){
         init: function(){
             if(!$('.eos-nodes').length) return;
             this.renderEos();
+            this.handleLoadBtn();
         },
         renderEos: function(){
+            var that = this;
             $.when($.get('https://www.chainnews.com/api/eos/')).then(function(data){
-                console.log(data);
+                $('.loading-box').addClass('hidden-box');
+                that.loadMore = data.next;
+                if(data.next){
+                    $('.load-more-btn').removeClass('hidden-box');
+                }
                 var banner = _.template($('#eos-nodes-banner-template').html());
                 var bannerHTML = banner({data: data.results.slice(0,3)});
                 $('.eos-banner').html(bannerHTML);
-                this.list = _.template($('#eos-nodes-list-template').html());
-                var listHTML = this.list({data: data.results.slice(3)});
-                $('.eos-list').html(listHTML);
+                that.list = _.template($('#eos-nodes-list-template').html());
+                that.listHTML = that.list({data: data.results.slice(3)});
+                $('.eos-list').html(that.listHTML);
             });
-
+        },
+        handleLoadBtn: function(){
+            var that = this;
+            $('.load-more-btn').click(function(){
+                $('.load-more-btn').html('加载中...').blur();
+                $.when($.get(that.loadMore)).then(function(data){
+                    that.loadMore = data.next;
+                    if(!data.next){
+                        $('.load-more-btn').addClass('hidden-box');
+                    }
+                    that.listHTML += that.list({data: data.results});
+                    $('.eos-list').html(that.listHTML);
+                    $('.load-more-btn').html('加载更多');
+                });
+            });
         }
     });
     return eosListApp;
--- static/js/web/jsbuild/index_page_app_build.js
+++ static/js/web/jsbuild/index_page_app_build.js
@@ -1414,7 +1414,7 @@ define('subapp/header/promo_text',['libs/Class','jquery', 'underscore'], functio
             //      }
             //
             // }, 1000);
-            this.element_count = 4 ;
+            this.element_count = 6 ;
             this.current_ele_index = 1 ;
             this.element_width = 150;
             this.$ele_wrapper = $('.promo-text-list');
@@ -8316,18 +8316,38 @@ define('subapp/eos/eos_list',['libs/Class', 'jquery', 'underscore'], function(Cl
         init: function(){
             if(!$('.eos-nodes').length) return;
             this.renderEos();
+            this.handleLoadBtn();
         },
         renderEos: function(){
+            var that = this;
             $.when($.get('https://www.chainnews.com/api/eos/')).then(function(data){
-                console.log(data);
+                $('.loading-box').addClass('hidden-box');
+                that.loadMore = data.next;
+                if(data.next){
+                    $('.load-more-btn').removeClass('hidden-box');
+                }
                 var banner = _.template($('#eos-nodes-banner-template').html());
                 var bannerHTML = banner({data: data.results.slice(0,3)});
                 $('.eos-banner').html(bannerHTML);
-                this.list = _.template($('#eos-nodes-list-template').html());
-                var listHTML = this.list({data: data.results.slice(3)});
-                $('.eos-list').html(listHTML);
+                that.list = _.template($('#eos-nodes-list-template').html());
+                that.listHTML = that.list({data: data.results.slice(3)});
+                $('.eos-list').html(that.listHTML);
             });
-
+        },
+        handleLoadBtn: function(){
+            var that = this;
+            $('.load-more-btn').click(function(){
+                $('.load-more-btn').html('加载中...').blur();
+                $.when($.get(that.loadMore)).then(function(data){
+                    that.loadMore = data.next;
+                    if(!data.next){
+                        $('.load-more-btn').addClass('hidden-box');
+                    }
+                    that.listHTML += that.list({data: data.results});
+                    $('.eos-list').html(that.listHTML);
+                    $('.load-more-btn').html('加载更多');
+                });
+            });
         }
     });
     return eosListApp;
