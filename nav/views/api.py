54616a4e01ac5259c1840e7a77e711b0cfd6cfba
Y4n <siyu@blocks.tech>
2018-05-17 17:01:09 +0800

    category view set

--- nav/views/api.py
+++ nav/views/api.py
@@ -2,8 +2,8 @@
 from braces.views import JSONResponseMixin
 from django.views.generic import TemplateView
 from rest_framework import generics, viewsets
-from nav.serializers import NavSerializer, NavDetailSerializer
-from nav.models import Nav
+from nav.serializers import NavSerializer, NavDetailSerializer, CategorySerializer
+from nav.models import Nav, Category
 from rest_framework import pagination
 
 
@@ -13,17 +13,10 @@ class StandardResultsSetPagination(pagination.PageNumberPagination):
     max_page_size = 1000
 
 
-# class NavListAPIView(generics.ListAPIView):
-#     serializer_class = NavSerializer
-#     queryset = Nav.objects.all()
-#     model = Nav
-
-
-# class CommonNavListAPIView(generics.ListCreateAPIView):
-#     serializer_class = NavDetailSerializer
-#     queryset = Nav.objects.all().order_by('-id')
-#     model = Nav
-#     pagination_class = StandardResultsSetPagination
+class CommonCateListViewSet(viewsets.ModelViewSet):
+    pagination_class = StandardResultsSetPagination
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all().order_by('-id')
 
 
 class CommonNavListViewSet(viewsets.ModelViewSet):
--- quark/urls/api.py
+++ quark/urls/api.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 from rest_framework.schemas import get_schema_view
 from rest_framework.urlpatterns import format_suffix_patterns
 
-from nav.views.api import CommonNavListViewSet, NavModelFieldsJsonView
+from nav.views.api import CommonNavListViewSet, NavModelFieldsJsonView, CommonCateListViewSet
 
 schema_view = get_schema_view(title='ChainNews API')
 
@@ -10,14 +10,15 @@ from rest_framework import routers
 
 router = routers.SimpleRouter()
 router.register(r'nav', CommonNavListViewSet)
-urlpatterns = router.urls
+router.register(r'cate', CommonCateListViewSet)
 
 # web api
 urlpatterns = [
-    url(r'^nav/meta_fields/?$', NavModelFieldsJsonView.as_view(), name='fields'),
-    url(r'^', include(router.urls, 'nav')),
+    # url(r'^nav/meta_fields/?$', NavModelFieldsJsonView.as_view(), name='fields'),
+    # url(r'^', include(router.urls, 'nav')),
     # api docs
     url(r'^schema/$', schema_view),
 ]
+urlpatterns += router.urls
 
 urlpatterns = format_suffix_patterns(urlpatterns)
