d2cecba4243f5d9791f629aa81da8dc130d36d6d
苏缓解 <suhuanjie@onedeMacBook-Pro.local>
2018-06-05 16:28:48 +0800

    test

--- advert/tests.py
+++ advert/tests.py
@@ -1,3 +1,58 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from advert.models import Advertisement
+from datetime import datetime
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from faker import Faker
+from factory.django import DjangoModelFactory
 
-# Create your tests here.
+f = Faker()
+
+
+class UserFactory(DjangoModelFactory):
+    class Meta:
+        model = User
+        django_get_or_create = ("email",)
+    email = f.email()
+
+
+class TestAdvertCase(APITestCase):
+    def setUp(self):
+        self.tearDown()
+        self.ad_name = f.name()
+        self.u = UserFactory()
+        self.obj1 = Advertisement.objects.create(
+            user=f.name(),
+            title=f.name(),
+            image='1.jpg',
+            link=f.url(),
+            updated_at=datetime.now(),
+        )
+        self.obj2 = Advertisement.objects.create(
+            user=f.name(),
+            title=f.name(),
+            image='2.jpg',
+            link=f.url(),
+            updated_at=datetime.now(),
+        )
+
+    def tearDown(self):
+        try:
+            ad = Advertisement.objects.all()
+            ad.delete()
+        except Advertisement.DoesNotExist:
+            pass
+
+    def test_advert(self):
+        token, _ = Token.objects.get_or_create(user=self.u)
+        self.u.is_superuser = True
+        self.u.save()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        _path = reverse('api:advertisement-list')
+        res = self.client.get(
+            path=_path,
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
